32cf643a13a4a94678531d1ea4e8b1ad165c89db
Corey Shanahan <coreyshanahan01@gmail.com>
2022-09-15 20:48:59 +0100

    Excercise 5

---
+++ exercise5.js
@@ -0,0 +1,12 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+    const uncompleted = json.reduce((total, todo, index, array) =>{
+      if(todo.completed === true) total.push({userId : todo.userId, title : todo.title})
+      return total
+    }, [])
+     console.log(uncompleted);
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
