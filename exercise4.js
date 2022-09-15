45f483982335d4046af2ccab671961ad47329121
Corey Shanahan <coreyshanahan01@gmail.com>
2022-09-15 21:10:39 +0100

    update

--- exercise4.js
+++ exercise4.js
@@ -2,7 +2,7 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
     const completeNum = json.reduce((total, todo, index, array) => {
-       return (todo.completed === false) ? total += 1 : total;
+       return (todo.completed) ? total += 1 : total;
     }, 0)
      console.log(completeNum);
   })
