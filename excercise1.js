43045c03f8f85dbd81b280b12db55ccf18f88e1f
Corey Shanahan <coreyshanahan01@gmail.com>
2022-09-15 15:56:36 +0100

    Exercise 1

---
+++ excercise1.js
@@ -0,0 +1,13 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+     const completed =  json.filter((todo,index) =>{
+        return todo.completed === true;
+     })// Complete this code
+     completed.forEach( (todo, index) => {
+      console.log(`${todo.title} - ${todo.completed}`)
+    })
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
